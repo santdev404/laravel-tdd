d6a265615286b2835a2292b63022881d58c531c9
santdev404 <santdev404@gmail.com>
2021-07-26 19:15:02 -0500

    Scoping projects

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,23 @@
 
 Route::get('/', function () {
     return view('welcome');
+
 });
 
-Route::get('/projects', 'ProjectsController@index');
+
+Route::group(['middleware' => 'auth'], function(){
+
+    Route::get('/projects', 'ProjectsController@index');
+
+    Route::post('/projects', 'ProjectsController@store');
+
+    Route::get('/projects/{project}', 'ProjectsController@show');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+});
 
 
-
-Route::post('/projects', 'ProjectsController@store')->middleware('auth');
-
-Route::get('/projects/{project}', 'ProjectsController@show');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
