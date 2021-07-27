d939930e8d4700ea73ba36518ee2ab5f2bec1f6b
santdev404 <santdev404@gmail.com>
2021-07-26 20:36:34 -0500

    Preparing frontend

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Tdd</h1>
+@extends ('layouts.app')
 
+@section('content')
+    
+    <div class="flex items-center">
+
+        
+        <a href="/projects/create">Create a new project</a>
+
+    </div>
+
+    <ul>
     @forelse ($projects as $project)
         <li>
             
@@ -21,6 +21,6 @@
         <li>No project yet</li>    
 
     @endforelse
+    </ul>
 
-</body>
-</html>
+@endsection
