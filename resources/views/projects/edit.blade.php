7cd3ea53ff35248560c75eb77a75bb05c85021d0
sant <santdev404@gmail.com>
2021-10-14 11:11:08 -0500

    reduce-form-duplication
    Add edit view

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,45 +1,21 @@
 
 @extends ('layouts.app')
 
+
+
 @section('content')
-    
-    
-    <form method="POST" action="{{$project->path()}}" >
 
-        @csrf
+<div class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow">
+    <h1 class="text-2xl font-normal mb-10 text-center">Edit a project</h1>
+
+    <form method="POST" action="{{ $project->path()}}">
         @method('PATCH')
+        @include('projects._form', [
+            'buttonText' => 'Update Project'
+            ])
 
-        <h1 class="haeding is-1">Edit your project</h1>
-
-        <div class="field">
-            <label class="label" for="title">Title</label>
-
-            <div class="control">
-                <input type="text" class="input" name="title" placeholder="" value="{{$project->title}}">
-            </div>
-        </div>
-            
-        <div class="field">
-
-            <label class="label" for="description">Description</label>
-
-            <div class="control">
-                <textarea  class="textarea" name="description" >{{$project->description}}</textarea>
-            </div>
-
-        </div>
-
-
-        <div class="field">
-            <div class="control">
-                <button type="submit" class="button is-link" >Edit project</button>
-                <a href="{{$project->path()}}">Cancel</a>
-            </div>
-        </div>
-
-
-
-        
     </form>
+    
+</div>
 
 @endsection
